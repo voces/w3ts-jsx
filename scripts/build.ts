cc64362714211615bcaa0d1e20bc241333100664
Robert Coe <chakrawc@gmail.com>
2020-11-27 15:08:10 -0800

    remove transformers

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,16 +1,18 @@
 import { emptyDir, copy } from "fs-extra";
 
+const testRegex = /test/;
+
 (async () => {
 	// Empty dist
 	await emptyDir("dist");
 
 	// Copy w3ts-jsx
-	await copy("src", "dist/src", { filter: (src) => !/test/.test(src) });
+	await copy("src", "dist/src", { filter: (path) => !testRegex.test(path) });
 
 	// Copy modules
 	await copy(
 		"node_modules/basic-pragma/src",
 		"dist/node_modules/basic-pragma/src",
-		{ filter: (src) => !/test/.test(src) },
+		{ filter: (path) => !testRegex.test(path) },
 	);
 })();
--- src/transformers.ts
+++
@@ -1,3 +0,0 @@
-/** @noSelfInFile **/
-
-export * from "tstl-jsx";
